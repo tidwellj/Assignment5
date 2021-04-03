b3ddfaa98a134606d879400b034017f307ff0a55
Jason Tidwell <68177963+tidwellj@users.noreply.github.com>
2021-04-03 00:10:26 -0500

    added star ratings

---
+++ Assets/Gold.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gold : MonoBehaviour
+{
+    //GameObject gold;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (SaveHighScoreJson.score == 3)
+            gameObject.SetActive(false);
+
+        Debug.Log(SaveHighScoreJson.score);
+       
+    }
+}
